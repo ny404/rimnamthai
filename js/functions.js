9c2ebb5d682c7de8efdcd9506301522393a8c971
unknown <nicholas.m.yeung@gmail.com>
2021-04-05 16:46:36 -0400

    sticky

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,30 @@
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// var navbar = document.getElementById("food-menu");
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// } 
+
+$(window).scroll(function() {
+    var distanceFromTop = $(this).scrollTop();
+    if (distanceFromTop >= $('.menu-cover').height()) {
+        $('#food-menu').addClass('sticky');
+    } else {
+        $('#food-menu').removeClass('sticky');
+    }
+});
+
 // Code By Webdevtrick ( https://webdevtrick.com )
 (function() {
 	var elements = document.querySelectorAll('img[data-src]');
